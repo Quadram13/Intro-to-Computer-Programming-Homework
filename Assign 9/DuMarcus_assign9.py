2bdcf711962a5fd0825016fe0e9c6ffb2e5db87b
Quadram13 <marcus.du@gmail.com>
2021-04-19 17:34:22 +0800

    1

--- Assign 9/DuMarcus_assign9.py
+++ Assign 9/DuMarcus_assign9.py
@@ -4,4 +4,20 @@ Marcus Du
 Section 006
 Part 1
 """
+filename=input("Enter a class file to grade (i.e. class1 for class1.txt): ")
+file_actual=filename+".txt"
+#try:
+file_object=open("class1.txt","r")
+alldata=file_object.read()
 
+print(alldata)
+
+file_object.close()
+
+
+
+"""except:
+    print("File cannot be found. ")
+else:
+    print("Successfully opened class1.txt")
+"""
